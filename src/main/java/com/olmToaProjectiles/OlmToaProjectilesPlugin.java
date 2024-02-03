dbcf492a66657b81ba1460889eae15c4aa17da38
kobeja <oharajacob@outlook.com>
2024-02-03 13:39:12 -0500

    config setup

--- src/main/java/com/olmToaProjectiles/OlmToaProjectilesPlugin.java
+++ src/main/java/com/olmToaProjectiles/OlmToaProjectilesPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.olmToaProjectiles;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Olm Toa Projectiles"
 )
-public class ExamplePlugin extends Plugin
+public class OlmToaProjectilesPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private OlmToaProjectilesConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    OlmToaProjectilesConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(OlmToaProjectilesConfig.class);
 	}
 }
--- src/test/java/com/olmToaProjectiles/OlmToaProjectilesPluginTest.java
+++ src/test/java/com/olmToaProjectiles/OlmToaProjectilesPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.olmToaProjectiles;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OlmToaProjectilesPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OlmToaProjectilesPlugin.class);
 		RuneLite.main(args);
 	}
 }
